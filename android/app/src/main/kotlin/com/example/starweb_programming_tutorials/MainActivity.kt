c01ae90deb590d616b3d232c94ef746fe2bcfd2b
Alabs <alabson.inc@gmail.com>
2020-03-02 18:14:02 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/starweb_programming_tutorials/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.starweb_programming_tutorials
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
